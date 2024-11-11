85c6b1502a10e14530215ac6dab72f41667f2f9a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-10 21:23:45 -0800

    Update Kebab-function.js

--- Kebab-function.js
+++ Kebab-function.js
@@ -26,3 +26,9 @@ function kebabize(str) {
 }
 
 // or
+
+function kebabize(str) {
+  return str.replace(/[^a-z]/ig, '').
+         replace(/^[A-Z]/, c => c.toLowerCase()).
+         replace(/[A-Z]/g, c => `-${c.toLowerCase()}`);
+}
