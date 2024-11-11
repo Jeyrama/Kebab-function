06bb1e66f84e5a2f6d6c0ada8ea2b8c5127d9554
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-10 21:23:18 -0800

    Update Kebab-function.js

--- Kebab-function.js
+++ Kebab-function.js
@@ -11,3 +11,16 @@ Notes:
 
 
 // Solution 
+
+function kebabize(str) {
+  const tmpStr = str.split("").filter(e => e.toLowerCase() !== e.toUpperCase()).join("")
+  let res = tmpStr[0].toLowerCase();
+  for(let i = 1; i < tmpStr.length; i++){
+    if(tmpStr[i].toUpperCase() == tmpStr[i]){
+      res = res + "-" + tmpStr[i].toLowerCase()
+    } else {
+      res += tmpStr[i]
+    }
+  }
+  return res;
+}
